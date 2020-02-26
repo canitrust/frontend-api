e24f4123f27e54dc9fcac0337dc418a56b46feb3
MinhNL <nguyenleminh530447@gmail.com>
2020-02-26 16:02:54 +0700

    Update lastest modified

--- controllers/Sitemap.js
+++ controllers/Sitemap.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
 /* ------------------------------------------------------------------------------------------------
  * Copyright (c) mgm security partners GmbH. All rights reserved.
@@ -10,41 +11,51 @@ const TestcaseService = require('../service/TestcaseService');
 
 module.exports.sitemapIndex = async (req, res) => {
   try {
-    const PathDefault = 'https://canitrust.in/';
-    const TagDefault = 'https://canitrust.in/tag/';
+    const PathDefault = 'https://www.canitrust.in/';
+    const TagDefault = 'https://www.canitrust.in/tag/';
     const obj = {
       urlset: {
         '@xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
         url: [],
       },
     };
-    const PathData = {
-      testcases: await TestcaseService.getAllPageTestcase(),
-    };
-    const homepageElement = {
-      loc: PathDefault,
-      lastmod: formatDate(
-        PathData.testcases[PathData.testcases.length - 1].date_created
-      ),
-      changefreq: 'Weekly',
-    };
-    obj.urlset.url.push(homepageElement);
-    for (const item of PathData.testcases) {
-      // pathList.push(item.path,item.date_created);
+    const PathData = await TestcaseService.getAllPageTestcase();
+    let date = new Date('January 1, 2018');
+    // Add testcase path in sitemap
+    for (const item of PathData) {
       const elementPath = {
         loc: PathDefault.concat(item.path),
-        lastmod: formatDate(item.date_created),
+        lastmod: item.date_created.toISOString().substring(0, 10),
         changefreq: 'Monthly',
       };
+      if (date <= item.date_created) date = item.date_created;
       obj.urlset.url.push(elementPath);
     }
-    const TagData = {
-      tags: await TagService.getAllTag(),
+    // Add hompage path in sitemap
+    const homepageElement = {
+      loc: PathDefault,
+      lastmod: date.toISOString().substring(0, 10),
+      changefreq: 'Weekly',
     };
-    for (const item of TagData.tags) {
+    obj.urlset.url.unshift(homepageElement);
+    // Add Tag path in sitemap
+    const TagData = await TagService.getAllTag();
+    for (const item of TagData) {
+      const arr = [];
+      // Check all testcases have same tag
+      for (let i = 0; i < PathData.length - 1; i++) {
+        const found = PathData[i].tagNums.find(function(element) {
+          return element === item.tagNumber;
+        });
+        if (found) arr.push(PathData[i].date_created);
+      }
+      let lastest = new Date('January 1, 2018');
+      for (let i = 0; i < arr.length; i++) {
+        if (lastest <= arr[i]) lastest = arr[i];
+      }
       const elementTag = {
         loc: TagDefault.concat(item.tagText),
-        lastmod: formatDate('Mon July 1,2019'),
+        lastmod: lastest.toISOString().substring(0, 10),
         changefreq: 'Monthly',
       };
       obj.urlset.url.push(elementTag);
@@ -59,15 +70,3 @@ module.exports.sitemapIndex = async (req, res) => {
     return res.status(400).json(null);
   }
 };
-
-function formatDate(date) {
-  const d = new Date(date);
-  let month = `${d.getMonth() + 1}`;
-  let day = `${d.getDate()}`;
-  const year = d.getFullYear();
-
-  if (month.length < 2) month = `0${month}`;
-  if (day.length < 2) day = `0${day}`;
-
-  return [year, month, day].join('-');
-}
