1d86c7c2b83ad94558739a21706bd804c143a6bc
MinhNL <nguyenleminh530447@gmail.com>
2020-03-02 17:54:46 +0700

    Update controller sitemap

--- controllers/Sitemap.js
+++ controllers/Sitemap.js
@@ -10,60 +10,59 @@ const TagService = require('../service/TagService');
 const TestcaseService = require('../service/TestcaseService');
 
 module.exports.sitemapIndex = async (req, res) => {
+  const PathDefault = 'https://www.canitrust.in/';
+  const TagDefault = 'https://www.canitrust.in/tag/';
+  const sitemap = {
+    urlset: {
+      '@xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
+      url: [],
+    },
+  };
+  const tagModified = [];
   try {
-    const PathDefault = 'https://www.canitrust.in/';
-    const TagDefault = 'https://www.canitrust.in/tag/';
-    const obj = {
-      urlset: {
-        '@xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
-        url: [],
-      },
-    };
-    const tagModified = [];
-    const PathData = await TestcaseService.getAllPageTestcase();
-    let date = new Date('January 1, 2018');
+    const testcaseData = await TestcaseService.getAllPageTestcase();
+    let testcaseLastModifiedDate = new Date('2018-01-01');
     // Add testcase path in sitemap
-    for (const item of PathData) {
-      const elementPath = {
+    for (const item of testcaseData) {
+      sitemap.urlset.url.push({
         loc: PathDefault.concat(item.path),
         lastmod: item.date_created.toISOString().substring(0, 10),
-        changefreq: 'Monthly',
-      };
+        changefreq: 'monthly',
+      });
       // Check some testcase have than one tags
-      for (let i = 0; i < item.tagNums.length; i++) {
-        // Check element if null then initialize
-        if (!tagModified[item.tagNums[i]])
-          tagModified[item.tagNums[i]] = new Date('January 1, 2018');
-        if (tagModified[item.tagNums[i]] < item.date_created)
-          tagModified[item.tagNums[i]] = item.date_created;
-      }
-      if (date <= item.date_created) date = item.date_created;
-      obj.urlset.url.push(elementPath);
+      item.tagNums.forEach((i) => {
+        if (!tagModified[i] || tagModified[i] < item.date_created)
+          tagModified[i] = item.date_created;
+      });
+      if (testcaseLastModifiedDate <= item.date_created)
+        testcaseLastModifiedDate = item.date_created;
     }
     // Add hompage path in sitemap
-    const homepageElement = {
+    sitemap.urlset.url.unshift({
       loc: PathDefault,
-      lastmod: date.toISOString().substring(0, 10),
-      changefreq: 'Weekly',
-    };
-    obj.urlset.url.unshift(homepageElement);
+      lastmod: testcaseLastModifiedDate.toISOString().substring(0, 10),
+      changefreq: 'weekly',
+    });
     // Add Tag path in sitemap
     const TagData = await TagService.getAllTag();
     for (const item of TagData) {
       // Check this tag has any testcase yet in case some tag don't have any testcase
       if (!tagModified[item.tagNumber])
-        tagModified[item.tagNumber] = new Date('January 1, 2018');
-      const elementTag = {
+        tagModified[item.tagNumber] = new Date('2018-01-01');
+      sitemap.urlset.url.push({
         loc: TagDefault.concat(item.tagText),
         lastmod: tagModified[item.tagNumber].toISOString().substring(0, 10),
-        changefreq: 'Monthly',
-      };
-      obj.urlset.url.push(elementTag);
+        changefreq: 'monthly',
+      });
     }
-    const feed = xmlbuilder.create(obj, { encoding: 'UTF-8' });
+    const feed = xmlbuilder.create(sitemap, { encoding: 'UTF-8' });
     const tmp = feed.end({ pretty: true });
     res.setHeader('Content-Type', 'application/xml');
-    res.removeHeader('Content-Security-Policy');
+    // res.removeHeader('Content-Security-Policy');
+    res.setHeader(
+      'Content-Security-Policy',
+      "default-src 'none'; style-src 'unsafe-inline'"
+    );
     return res.status(200).send(tmp);
   } catch (error) {
     console.log(error);
